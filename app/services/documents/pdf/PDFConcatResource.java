6685d0e5125071b0724c1a66a2d4a1cb4cfa78f6
Jeff Quinn <jeff@nuna.com>
2017-08-21 22:19:03 -0700

    copied over code

---
+++ app/services/documents/pdf/PDFConcatResource.java
@@ -0,0 +1,41 @@
+package services.documents.pdf;
+
+import com.codahale.metrics.annotation.Timed;
+import com.google.common.collect.Lists;
+import org.apache.log4j.Logger;
+import org.glassfish.jersey.media.multipart.FormDataBodyPart;
+import org.glassfish.jersey.media.multipart.FormDataMultiPart;
+import services.documents.pdf.PDFConcatenation;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.io.InputStream;
+import java.util.ArrayList;
+
+/**
+ * Resource for concatenating multiple pdf files.
+ */
+@Path("/concat")
+@Produces("application/pdf")
+public class PDFConcatResource {
+    private static final Logger logger = Logger.getLogger(PDFConcatResource.class);
+
+    @POST
+    @Timed
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    public Response createPDF(FormDataMultiPart multiPart) {
+        ArrayList<InputStream> pdfs = Lists.newArrayList();
+        logger.info("Received multipart:" + multiPart.getFields().toString());
+
+        for (FormDataBodyPart part : multiPart.getFields("attachments")) {
+            pdfs.add(part.getValueAs(InputStream.class));
+        }
+
+        logger.info("Received " + pdfs.size() + " pdfs to concat");
+        return Response.ok(new PDFConcatenation(pdfs)).build();
+    }
+}
---
+++ test/services/documents/pdf/PDFStampingTest.java
@@ -0,0 +1,92 @@
+package gov.va.vetaffi;
+
+import com.google.common.base.Throwables;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.pdf.AcroFields;
+import com.itextpdf.text.pdf.PdfReader;
+import com.itextpdf.text.pdf.PdfStamper;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.io.output.NullOutputStream;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+public class PDFStampingTest {
+
+    private static String SIGNATURE = null;
+    static {
+        try {
+            SIGNATURE = IOUtils.toString(PDFStampingTest.class.getClassLoader().getResourceAsStream("test_image"));
+        } catch (IOException e) {
+            Throwables.propagate(e);
+        }
+    }
+
+    @Rule
+    public TemporaryFolder TMP = new TemporaryFolder();
+
+    @Test
+    public void testStampPdfWithCheckAndText() throws Exception {
+        InputStream pdfTemplate =
+                PDFStreamingOutput.class.getClassLoader().getResourceAsStream("forms/VBA-21-4502-ARE.pdf");
+        File tmpFile = TMP.newFile("test.pdf");
+
+        HashMap<String, String> idMap = Maps.newHashMap();
+        idMap.put("Army", "ARMY[0]");
+        PDFStamping.stampPdf(pdfTemplate,
+                Lists.newArrayList(
+                        new PDFField("first_name", "jeff"),
+                        new PDFField("branch", "Army"),
+                        new PDFField("street", "68 Mt. Vernon St.")
+                ),
+                Lists.newArrayList(
+                        new PDFFieldLocator("namefirst1[0]", "first_name", 0, null, null, null, false),
+                        new PDFFieldLocator(null, "branch", 0, idMap, null, null, false),
+                        new PDFFieldLocator("form1[0].Page_3[0].namelast1[14]", "street", 0, null, null, null, false)
+                ),
+                new FileOutputStream(tmpFile));
+    }
+
+    @Test
+    public void testStampWithSignature() throws Exception {
+        InputStream pdfTemplate =
+                PDFStreamingOutput.class.getClassLoader().getResourceAsStream("forms/VBA-21-0966-ARE.pdf");
+        File tmpFile = TMP.newFile("test.pdf");
+
+        HashMap<String, String> idMap = Maps.newHashMap();
+        idMap.put("Army", "ARMY[0]");
+        PDFStamping.stampPdf(pdfTemplate,
+                Lists.newArrayList(
+                        new PDFField("signature", SIGNATURE)
+                ),
+                Lists.newArrayList(
+                        new PDFFieldLocator("F[0].Page_1[0].SignatureField1[0]",
+                                "signature", 0, null, null, null, true)
+                ),
+                new FileOutputStream(tmpFile));
+    }
+
+    @Test
+    public void testReadAcroForm() throws Exception {
+        InputStream pdfTemplate =
+                PDFStreamingOutput.class.getClassLoader().getResourceAsStream("forms/VBA-21-0966-ARE.pdf");
+        PdfReader reader = new PdfReader(pdfTemplate);
+        PdfStamper stamper;
+        try {
+            stamper = new PdfStamper(reader, new NullOutputStream());
+        } catch (DocumentException e) {
+            throw Throwables.propagate(e);
+        }
+        AcroFields form = stamper.getAcroFields();
+        Map<String, AcroFields.Item> fields = form.getFields();
+    }
+}
