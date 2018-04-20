bab50cd9012c38633875a4b35ba95f220cd49a7b
vetafi-admin <31091265+vetafi-admin@users.noreply.github.com>
2018-04-19 21:34:33 -0700

    Add Gzip Filter (#8)

--- app/Filters.scala
+++ app/Filters.scala
@@ -4,6 +4,7 @@ import com.digitaltangible.playguard.GuardFilter
 import filters.{ AccessLogFilter, HttpsRedirectFilter }
 import play.api.http.DefaultHttpFilters
 import play.filters.csrf.CSRFFilter
+import play.filters.gzip.GzipFilter
 import play.filters.headers.SecurityHeadersFilter
 import play.filters.hosts.AllowedHostsFilter
 
@@ -13,10 +14,12 @@ class Filters @Inject() (
   securityHeadersFilter: SecurityHeadersFilter,
   guardFilter: GuardFilter,
   httpsRedirectFilter: HttpsRedirectFilter,
+  gzipFilter: GzipFilter,
   accessLogFilter: AccessLogFilter) extends DefaultHttpFilters(
   csrfFilter,
   allowedHostsFilter,
   securityHeadersFilter,
   guardFilter,
   httpsRedirectFilter,
+  gzipFilter,
   accessLogFilter)
