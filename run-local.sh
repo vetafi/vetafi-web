fac317766edd80a33cec0d050573320356b6836d
vetafi-admin <31091265+vetafi-admin@users.noreply.github.com>
2018-05-30 08:38:14 -0700

    Fix Content Security Policy (#17)
    * done
    
    * in progress
    {
    
    * fix issue
    
    * fix scripts

--- run-local.sh
+++ run-local.sh
@@ -4,4 +4,10 @@ set -eux -o pipefail
 export VETAFI_CLIENT_ID="$(biscuit get --filename=conf/biscuit/secrets.yaml local::id-me-client-id)"
 export VETAFI_CLIENT_SECRET="$(biscuit get --filename=conf/biscuit/secrets.yaml local::id-me-client-secret)"
 
-sbt run -Dconfig.resource=application.local.conf -Dlogger.resource=logback.xml
+cd ui
+
+npm run-script build-prod
+
+cd ..
+
+sbt clean run -Dconfig.resource=application.local.conf -Dlogger.resource=logback.xml
